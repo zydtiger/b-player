e233b53b98c7fecbf57fdbc4bb77bcb65e61fa62
tigerding <zhiyuanding01@gmail.com>
2025-12-20 16:29:10 -0800

    add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@@": path.resolve(__dirname, "./"),
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
