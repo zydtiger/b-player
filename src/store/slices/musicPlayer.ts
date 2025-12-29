a661f137ac34794bda1120b770d5c087d1d32507
tigerding <zhiyuanding01@gmail.com>
2025-12-29 15:02:55 -0800

    Update musicPlayer.ts

--- src/store/slices/musicPlayer.ts
+++ src/store/slices/musicPlayer.ts
@@ -4,12 +4,10 @@ import { MusicPiece } from "@@/shared/model";
 
 export interface MusicPlayerState {
   currentMusic?: MusicPiece;
-  isPlaying: boolean;
 }
 
 const initialState: MusicPlayerState = {
   currentMusic: undefined,
-  isPlaying: false,
 };
 
 const musicPlayerSlice = createSlice({
@@ -19,12 +17,9 @@ const musicPlayerSlice = createSlice({
     setCurrentMusic(state, action: PayloadAction<MusicPiece>) {
       state.currentMusic = action.payload;
     },
-    setIsPlaying(state, action: PayloadAction<boolean>) {
-      state.isPlaying = action.payload;
-    },
   },
 });
 
-export const { setCurrentMusic, setIsPlaying } = musicPlayerSlice.actions;
+export const { setCurrentMusic } = musicPlayerSlice.actions;
 
 export default musicPlayerSlice.reducer;
