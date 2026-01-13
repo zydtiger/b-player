8ac6aebfe8840973bf019f5e90989c5183d86b0d
tigerding <zhiyuanding01@gmail.com>
2026-01-13 15:38:07 +0800

    Add startup background to reduce flicker

--- electron/main.ts
+++ electron/main.ts
@@ -36,6 +36,7 @@ let win: BrowserWindow | null;
 function createWindow() {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    backgroundColor: "#242424",
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs"),
     },
